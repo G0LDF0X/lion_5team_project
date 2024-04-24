12cb777ddd5434d82d1919e98c5cca511bcf6c85
InnovationPARK <innovation0220@gmail.com>
2024-04-24 17:35:11 +0900

    modify model

--- be/app/models.py
+++ be/app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User as auth_user
 
 # Create your models here.
+
+
 class User(models.Model):
     user_id = models.ForeignKey(auth_user, on_delete=models.CASCADE)
     name = models.CharField('name', max_length=100)
@@ -110,8 +112,8 @@ class Follow(models.Model):
 
 class Item_QnA(models.Model):
     # 외래키
-    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
 
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
@@ -120,9 +122,9 @@ class Item_QnA(models.Model):
 
 class Item_Answer(models.Model):
     # 외래키
-    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
-    item_qna_id = models.ForeignKey(Item_QnA, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
+    item_qna = models.ForeignKey(Item_QnA, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
@@ -133,17 +135,17 @@ class Board(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     product_url = models.CharField(max_length=200)
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
+    tile = models.CharField(max_length=100)
+    content = models.TextField()
     image_url = models.ImageField()
-    show = models.IntegerField(default=0)
-    like = models.IntegerField(default=0)
+    show = models.IntegerField(default="0")
+    like = models.IntegerField(default="0")
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Reply(models.Model):
     # 외래키
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     
     content = models.TextField()
     replied_id = models.IntegerField()
@@ -155,3 +157,4 @@ class Bookmark(models.Model):
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
