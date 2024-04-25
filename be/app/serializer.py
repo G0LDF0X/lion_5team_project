a06686134d3ec2b225d58d316a1b7d6d46f76aa0
parksungjoon0805 <jjoon9908@naver.com>
2024-04-25 14:36:13 +0900

    QnA Connect

--- be/app/serializer.py
+++ be/app/serializer.py
@@ -11,3 +11,28 @@ class ItemSerializer(serializers.ModelSerializer):
     
     def get_qna(self, obj):
         return obj.qna_set.all()
+    
+
+
+class UserQnASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_QnA
+        fields = '__all__'  
+    
+    def get_qna(self, obj):
+        return obj.qna_set.all()
+    
+
+class UserAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Answer
+        fields = '__all__'
+
+    def get_user_qna(self, obj):
+        return obj.user_qna_set.all()
+    
+    def get_user_answer(self, obj):
+        return obj.user_answer_set.all()
+    
+    def get_user(self, obj):
+        return obj.user.username
--- be/backend/urls.py
+++ be/backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('app.urls.items_urls')),
+    path('qna/', include('app.urls.qna_urls')),
 ]
