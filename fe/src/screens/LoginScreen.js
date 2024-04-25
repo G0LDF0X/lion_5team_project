a2072e0c57f670523f180c201de2c49d5afeff55
shin <sinhyeonseo1026@g.eulji.ac.kr>
2024-04-25 14:12:50 +0900

    hyeonseo

--- fe/src/screens/LoginScreen.js
+++ fe/src/screens/LoginScreen.js
@@ -92,7 +92,7 @@ function LoginScreen() {
       <Row className="py-3">
         <Col>
           New Customer?{" "}
-          <Link to={redirect ? `/register?redirect=${redirect}` : `/register`}>
+          <Link to={redirect ? `/register/` : `/register`}>
             Register
           </Link>
         </Col>
