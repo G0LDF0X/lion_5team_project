b352d32e6c65d8d9ac1d63f3e8e47f77300c2271
parksungjoon0805 <jjoon9908@naver.com>
2024-04-26 14:05:05 +0900

    Update qna_url

--- be/app/urls/qna_urls.py
+++ be/app/urls/qna_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.views.qna_views import qna_board, qna_detail, create_user_qna, update_user_qna, delete_user_qna, create_user_answer, update_user_answer, delete_user_answer
+from app.views.qna_views import qna_board, qna_detail, create_user_qna, update_user_qna, delete_user_qna
 
 urlpatterns = [
     path('', qna_board, name='qna_board'),  # Q&A 전체보기
@@ -7,7 +7,4 @@ urlpatterns = [
     path('create/', create_user_qna, name='create_user_qna'),  # Q&A 생성
     path('update/<int:pk>/', update_user_qna, name='update_user_qna'),  # Q&A 수정
     path('delete/<int:pk>/', delete_user_qna, name='delete_user_qna'),  # Q&A 삭제
-    path('answer/create/', create_user_answer, name='create_user_answer'),  # 답변 생성
-    path('answer/update/<int:pk>/', update_user_answer, name='update_user_answer'),  # 답변 수정
-    path('answer/delete/<int:pk>/', delete_user_answer, name='delete_user_answer'),  # 답변 삭제
 ]
