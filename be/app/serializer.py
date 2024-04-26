3a8bdc2dee71f9e87e70abc6a59a0adeafa739a2
G0LDF0X <astnv31@gmail.com>
2024-04-26 10:54:49 +0900

    modify resgister

--- be/app/serializer.py
+++ be/app/serializer.py
@@ -50,7 +50,7 @@ class UserAnswerSerializer(serializers.ModelSerializer):
     def get_user(self, obj):
         return obj.user.username
     
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as auth_user
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
@@ -73,7 +73,7 @@ class UserSerializer(serializers.ModelSerializer):
     isseller = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
-        model = User
+        model = auth_user
         fields = '__all__'
     
     def get_username(self, obj):
@@ -113,8 +113,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = User
-        fields = ('username', 'password', 'password2')
+        model = auth_user
+        fields = ('username', 'email', 'password', 'password2')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -124,9 +124,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username']
+        user = auth_user.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
         )
 
         user.set_password(validated_data['password'])
         user.save()
+        return user
