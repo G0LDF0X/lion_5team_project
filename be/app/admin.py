71daad8f1787fafa2c6b5630c0b4a6915f97d9d5
parksungjoon0805 <jjoon9908@naver.com>
2024-04-29 11:56:47 +0900

    admin.py update

--- be/app/admin.py
+++ be/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Seller,Board,Tag,Category,User,User_QnA,User_Answer,Item, Review, Reply, Item_Answer, Item_QnA
+from .models import Seller,Board,Tag,Category,User,User_QnA,User_Answer, Item, Review, Item_QnA, Item_Answer, Reply
 admin.site.register(Board)
 admin.site.register(Seller)
 admin.site.register(Tag)
@@ -9,7 +9,7 @@ admin.site.register(User_QnA)
 admin.site.register(User_Answer)
 admin.site.register(Item)
 admin.site.register(Review)
-admin.site.register(Reply)
-admin.site.register(Item_Answer)
 admin.site.register(Item_QnA)
+admin.site.register(Item_Answer)
+admin.site.register(Reply)
 # Register your models here.
