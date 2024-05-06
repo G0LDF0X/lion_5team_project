6cabaaa3745b9e742c4fd964b7eaa8c418a3b764
shin <sinhyeonseo1026@g.eulji.ac.kr>
2024-05-06 15:29:58 +0900

    Item_Detail -> Item_Q&A

--- fe/src/components/QA.js
+++ fe/src/components/QA.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-function QA({ qna }) {
-    // if (!qa) return <div>QA not found</div>;
+function QA({ qna={} }) {
+    // if (!qna) return <div>QA not found</div>;
     return (
             <div>
          {/* <Card className="my-3 p-3 rounded"> */}
-            <Link to={`/items/detail/${qna.id}`}> 
+            {<Link to={`/items/detail/${qna.id}`}> 
                 {/* <Card.Img src={qa.image_url} variant="top" /> */}
-            </Link>
+            </Link> }
 
             <Card.Body>
                 <Link to={`/qna/detail/${qna.id}`}> 
