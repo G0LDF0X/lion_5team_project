a63af7695e08abefaa66f6fbb1f4878c6dcc40d9
G0LDF0X <astnv31@gmail.com>
2024-05-06 16:23:19 +0900

    change sorting in bookmark.js

--- fe/src/components/Bookmark.js
+++ fe/src/components/Bookmark.js
@@ -24,35 +24,33 @@ const Bookmark = () => {
   return (
     <div>
       <h2>나의 북마크</h2>
-      <div className='d-flex flex-row flex-wrap'>
-        {loading ? (
-          <Loading />
-        ) : error ? (
-          <Message variant={"danger"}>{error}</Message>
-        ) : (
-          <div>
-      {bookMarkItems.map((bookmark) => (
-        <Card style={{ width: '18rem' , margin: '10px' }} key={bookmark.id}>
-        <Card.Body>
-          <Card.Title>{bookmark.name}</Card.Title>
-          <Card.Subtitle className='mb-2 text-muted'>{bookmark.price}원</Card.Subtitle>
-          <Badge bg="info">{bookmark.category}</Badge>
-          <Card.Text>
-            <br />
-            {bookmark.description&&bookmark.description.length > 30 
-                ? `${bookmark.description.substring(0, 30)}...` 
-                : bookmark.description}
-            </Card.Text>
-          <Link to={`/items/detail/${bookmark.item_id}`}>
-          <Button variant="primary">바로가기</Button>
-          </Link>
-        </Card.Body>   
-      </Card>
-      )
-      )}</div>)}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant={"danger"}>{error}</Message>
+      ) : (
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {bookMarkItems.map((bookmark) => (
+            <Card style={{ width: '18rem' , margin: '10px' }} key={bookmark.id}>
+              <Card.Body>
+                <Card.Title>{bookmark.name}</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>{bookmark.price}원</Card.Subtitle>
+                <Badge bg="info">{bookmark.category}</Badge>
+                <Card.Text>
+                  <br />
+                  {bookmark.description&&bookmark.description.length > 30 
+                      ? `${bookmark.description.substring(0, 30)}...` 
+                      : bookmark.description}
+                </Card.Text>
+                <Link to={`/items/detail/${bookmark.item_id}`}>
+                  <Button variant="primary">바로가기</Button>
+                </Link>
+              </Card.Body>   
+            </Card>
+          ))}
+        </div>
+      )}
     </div>
-    
   );
 };
 
