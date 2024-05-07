3584b00c87693f629b3a8d871ac6623c2025226a
G0LDF0X <astnv31@gmail.com>
2024-05-07 11:54:04 +0900

    modify OtherUserProfileMain.js

--- fe/src/components/OtherUserProfileMain.js
+++ fe/src/components/OtherUserProfileMain.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import OtherUserProfileCard from './OtherUserProfileCard';
 import { Card, Row, Col } from 'react-bootstrap'
-import axios from 'axios';
 
 function OtherUserProfileMain({ userId }) {
     return (
