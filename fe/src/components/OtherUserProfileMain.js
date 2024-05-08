751d652a1ca11b458622c591f4d4500774b1e5db
InnovationPARK <innovation0220@gmail.com>
2024-05-08 17:24:19 +0900

    push profile follow

--- fe/src/components/OtherUserProfileMain.js
+++ fe/src/components/OtherUserProfileMain.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
 import OtherUserProfileCard from './OtherUserProfileCard';
 import { Card, Row, Col } from 'react-bootstrap'
 
 function OtherUserProfileMain({ userId }) {
     return (
         <div>
+            <h1>Other User Profile Main123123</h1>
             <Row className='justify-content-start'>
                 <OtherUserProfileCard userId={userId} />
             </Row>
         </div>
+        
     )
 }
 
