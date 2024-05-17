baba34e1f7ff4f090db09d9a4ce31fca2dec39d8
YechanJung <godofphysics0314@gmail.com>
2024-05-17 14:44:35 +0900

    search query

--- be/fastapi_app/main.py
+++ be/fastapi_app/main.py
@@ -1,30 +1,23 @@
+
 import os
-import django
+import pandas as pd
 from fastapi import FastAPI, HTTPException
-from asgiref.sync import sync_to_async
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-
-django.setup()
-
-
-from app.models import Item
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma
 
 app = FastAPI()
 
-@app.get("/test/items/{item_id}")
-async def read_item(item_id: int):
-    try:
-        item = await sync_to_async(Item.objects.get)(id=item_id)
-        print(f"Item found: {item}")
-        return {"item_id": item.id, "name": item.name}
-    except Item.DoesNotExist:
-        print("Item not found.")
-        raise HTTPException(status_code=404, detail="Item not found")
-    except Exception as e:
-        print("Error retrieving item:", str(e))
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-    
 
-    # uvicorn fastapi_app.main:app --reload --port 8002
+file_path = os.path.join(os.path.dirname(__file__), 'output.csv')
+items = pd.read_csv(file_path)
+
+sbert = SentenceTransformerEmbeddings(model_name='jhgan/ko-sroberta-multitask')
+vector_store = Chroma.from_texts(
+    texts=items['name'].tolist(),
+    embedding=sbert
+)
+
+@app.post("/search/")
+def search_books(query: str):
+    results = vector_store.similarity_search(query=query, k=3)
+    return {"query": query, "results": results}
