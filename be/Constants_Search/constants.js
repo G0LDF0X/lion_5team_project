15b3028c3e4bb40f4fd5413899458f814d0c5d1c
YechanJung <127584126+YechanJung@users.noreply.github.com>
2024-06-01 20:32:40 +0900

    update

--- be/Constants_Search/constants.js
+++ be/Constants_Search/constants.js
@@ -1,38 +1,37 @@
-import express from 'express';
-import { chosungIncludes } from 'es-hangul';
-import fs from 'fs';
-import csv from 'csv-parser';
+import express from "express";
+import { chosungIncludes } from "es-hangul";
+import fs from "fs";
+import csv from "csv-parser";
 
 var app = express();
-const data = []
+const data = [];
 
-fs.createReadStream('../fastapi_app/items.csv')  .pipe(csv())
-  .on('data', (row) => {
+fs.createReadStream("../fastapi_app/items.csv")
+  .pipe(csv())
+  .on("data", (row) => {
     data.push(row);
   })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
+  .on("end", () => {
+    console.log("CSV file successfully processed");
   });
 
 function search(query) {
-    const results = [];
-    for (const item of data) {
-        console.log(item.name);
-        if (chosungIncludes(item.name, query)) {
-            results.push(item.name);
-        }
+  const results = [];
+  for (const item of data) {
+    console.log(item.name);
+    if (chosungIncludes(item.name, query)) {
+      results.push(item.name);
     }
-    return results;
+  }
+  return results;
 }
 
-app.get('/search', (req, res) => {
-    const query = req.query.q;
-    const results = search(query);
-    res.send(results);
-    }
-)
+app.get("/search", (req, res) => {
+  const query = req.query.q;
+  const results = search(query);
+  res.send(results);
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-)
+  console.log("Server is running on port 3000");
+});
