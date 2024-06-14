49731272971dbe1e4afd3b741d05304788f7ba21
InnovationPARK <innovation0220@gmail.com>
2024-06-14 16:09:53 +0900

    mail success

--- be/backend/settings.py
+++ be/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -219,18 +221,27 @@ MEDIA_ROOT = 'static/images'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# AUTH_USER_MODEL = 'app.User'
+
 LOGIN_REDIRECT_URL = '/login/'
+
 LOGOUT_REDIRECT_URL = '/'
 
-
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST ='smtp.gmail.com'
+
 EMAIL_PORT = 587
+
+load_dotenv()
+EMAIL_HOST_USER =os.getenv('MY_EMAIL')
+
+EMAIL_HOST_PASSWORD =os.getenv('MY_EMAIL_PASSWORD')
+
 EMAIL_USE_TLS = True
+
 EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER =''
-EMAIL_HOST_PASSWORD =''
-
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 SERVER_EMAIL = EMAIL_HOST_USER
