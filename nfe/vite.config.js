26a6ea576ee0e3f522958051ff75351edf93b067
YechanJung <127584126+YechanJung@users.noreply.github.com>
2024-06-18 15:13:17 +0900

    update

--- nfe/vite.config.js
+++ nfe/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import polyfillNode from 'rollup-plugin-polyfill-node';
 import viteCompression from 'vite-plugin-compression';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
@@ -15,5 +16,20 @@ export default defineConfig({
       algorithm: 'brotliCompress',
       ext: '.br',
     }),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+    }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
