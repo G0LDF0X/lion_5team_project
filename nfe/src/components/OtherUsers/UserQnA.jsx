2407b94bd08cae3918a56d8c7341329520927445
parksungjoon0805 <jjoon9908@naver.com>
2024-06-20 15:38:25 +0900

    Other Profile QnA

--- nfe/src/components/OtherUsers/UserQnA.jsx
+++ nfe/src/components/OtherUsers/UserQnA.jsx
@@ -4,50 +4,85 @@ import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const UserQnA = ({ userId, userDetail }) => {
-  const [qnaItems, setQnaItems] = useState([]);
+  const [userQnAs, setUserQnAs] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
   useEffect(() => {
     mainAxiosInstance.get(`/qna/${userId}/qna/`)
       .then(response => {
-        console.log('QnA Response Data:', response.data);
-        setQnaItems(response.data);
+        const data = response.data;
+        
+        // 질문을 최신순으로 정렬
+        data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+        
+        setUserQnAs(data);
       })
       .catch(error => console.error('Error:', error));
   }, [userId]);
 
+  useEffect(() => {
+    mainAxiosInstance.get(`/qna/${userId}/answers/`)
+      .then(response => {
+        const data = response.data;
+
+        // 답변을 최신순으로 정렬
+        data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+        
+        setUserAnswers(data);
+      })
+      .catch((error) => console.error('Error:', error));
+  }, [userId]);
+
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto py-8">
       <Typography variant="h4" className="mb-8 font-bold text-gray-800">
         {userDetail.nickname}의 질문과 답변
       </Typography>
-      <div className="flex flex-wrap">
-        {qnaItems && qnaItems.length > 0 ? (
-          qnaItems.map((qna) => (
-            <div
-              key={qna.question.id}
-              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2"
-            >
-              <Link to={`/qna/detail/${qna.question.id}`}>
-              <div className="bg-white shadow-md rounded-lg overflow-hidden">
-                <div className="p-4">
-                  <h2 className="text-xl font-semibold mb-2">{qna.question.title}</h2>
-                  {qna.answers.map((answer) => (
-                    <p key={answer.id} className="text-gray-700 mt-2">
-                      {answer.content}
-                    </p>
-                  ))}
-                </div>
-              </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <h2 className="text-2xl font-bold mb-4">{userDetail.nickname}의 질문</h2>
+          {userQnAs && userQnAs.map((userQnA, index) => (
+            <div key={index} className="bg-white shadow-md rounded-lg mb-4 p-4">
+              <Link to={`/qna/detail/${userQnA.id}`}>
+                <h3 className="text-lg font-semibold">{userQnA.title}</h3>
               </Link>
+              {userQnA.content.length > 100 ? (
+                <div
+                  dangerouslySetInnerHTML={{ __html: `${userQnA.content.substring(0, 100)}...` }}
+                  className="text-black bg-white mt-2"
+                />
+              ) : (
+                <div
+                  dangerouslySetInnerHTML={{ __html: userQnA.content }}
+                  className="text-black bg-white mt-2"
+                />
+              )}
+              <small className="text-gray-500 mt-2 block">{userQnA.created_at.split('T')[0]}</small>
             </div>
-          ))
-        ) : (
-          <div className="w-full p-4 text-center">
-            <h1 className="text-center text-2xl font-bold text-gray-700">
-              등록된 질문과 답변이 없습니다.
-            </h1>
-          </div>
-        )}
+          ))}
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold mb-4">{userDetail.nickname}의 답변</h2>
+          {userAnswers && userAnswers.map((userAnswer, index) => (
+            <div key={index} className="bg-white shadow-md rounded-lg mb-4 p-4">
+              <Link to={`/qna/detail/${userAnswer.user_qna_id}`}>
+                <h3 className="text-lg font-semibold">{userAnswer.title}</h3>
+              </Link>
+              {userAnswer.content.length > 100 ? (
+                <div
+                  dangerouslySetInnerHTML={{ __html: `${userAnswer.content.substring(0, 100)}...` }}
+                  className="text-black bg-white mt-2"
+                />
+              ) : (
+                <div
+                  dangerouslySetInnerHTML={{ __html: userAnswer.content }}
+                  className="text-black bg-white mt-2"
+                />
+              )}
+              <small className="text-gray-500 mt-2 block">{userAnswer.created_at.split('T')[0]}</small>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
