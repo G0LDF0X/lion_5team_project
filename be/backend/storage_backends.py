cb022811a00c89b3b7c79437bbceabbac1ec0de0
InnovationPARK <innovation0220@gmail.com>
2024-06-20 17:54:38 +0900

    6

---
+++ be/backend/storage_backends.py
@@ -0,0 +1,11 @@
+# storage_backends.py
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
